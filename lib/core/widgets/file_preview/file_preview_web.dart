32d358bb5b967f59eb6fe19c55b14734b3d8ea56
thienkk25 <thiennguyendz25@gmail.com>
2026-06-03 22:18:29 +0700

    feat: implement FilePreviewWidget for web using iframe-based document rendering

--- lib/core/widgets/file_preview/file_preview_web.dart
+++ lib/core/widgets/file_preview/file_preview_web.dart
@@ -13,6 +13,32 @@ class FilePreviewWidget extends StatefulWidget {
 class _FilePreviewWidgetState extends State<FilePreviewWidget> {
   late final String viewId;
 
+  String _getPreviewUrl(String originalUrl) {
+    try {
+      final uri = Uri.parse(originalUrl);
+      final path = uri.path.toLowerCase();
+      if (path.endsWith('.docx') ||
+          path.endsWith('.doc') ||
+          path.endsWith('.xlsx') ||
+          path.endsWith('.xls') ||
+          path.endsWith('.pptx') ||
+          path.endsWith('.ppt')) {
+        return 'https://view.officeapps.live.com/op/embed.aspx?src=${Uri.encodeComponent(originalUrl)}';
+      }
+    } catch (_) {
+      final lowerUrl = originalUrl.toLowerCase();
+      if (lowerUrl.contains('.docx') ||
+          lowerUrl.contains('.doc') ||
+          lowerUrl.contains('.xlsx') ||
+          lowerUrl.contains('.xls') ||
+          lowerUrl.contains('.pptx') ||
+          lowerUrl.contains('.ppt')) {
+        return 'https://view.officeapps.live.com/op/embed.aspx?src=${Uri.encodeComponent(originalUrl)}';
+      }
+    }
+    return originalUrl;
+  }
+
   @override
   void initState() {
     super.initState();
@@ -21,7 +47,7 @@ class _FilePreviewWidgetState extends State<FilePreviewWidget> {
       viewId,
       (int viewId) {
         final iframe = web.HTMLIFrameElement()
-          ..src = widget.url
+          ..src = _getPreviewUrl(widget.url)
           ..style.border = 'none'
           ..style.width = '100%'
           ..style.height = '100%';
